6c1bb1ec80e9ee59b57f2da58798150341cb9b89
levi <levidousseaux@gmail.com>
2020-12-03 22:05:17 -0300

    test: return null when user is not found

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,15 +1,15 @@
-import { UserData } from "./user-data"
+import { UserData } from './user-data'
 
-describe('Register user on mailing list use case', () =>    {
-    test('should add user with complete data to mailing list', async () =>  {
-        const users: UserData[] = []
-        console.log(users)
-        // const repo: UserRepository = new InMemoryUserRepository(users)
-        // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
-        // const name = 'name'
-        // const email = 'name@email.com'
-        // const response = await usecase.registerUserOnMailingList({ name, email })
-        // const user = repo.findUserByEmail('name@email.com')
-        // expect((await user).name).toBe('name')
-    })
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    // const name = 'name'
+    // const email = 'name@email.com'
+    // const response = await usecase.registerUserOnMailingList({ name, email })
+    // const user = repo.findUserByEmail('name@email.com')
+    // expect((await user).name).toBe('name')
+  })
 })
--- src/usecases/register-user-on-mailing-list/user-data.ts
+++ src/usecases/register-user-on-mailing-list/user-data.ts
@@ -1,4 +1,4 @@
-export interface UserData   {
+export interface UserData {
     name: string
     email: string
 }
