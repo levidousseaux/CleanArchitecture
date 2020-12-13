ad14784a79a298ef7d1be614e594626409171a96
levi <levidousseaux@gmail.com>
2020-12-13 09:24:07 -0300

    docs: add email and name validation

--- src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
+++ src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository.ts
@@ -20,7 +20,7 @@ export class InMemoryUserRepository implements UserRepository {
     return found || null
   }
 
-  async findAllUser (): Promise<UserData[]> {
+  async findAllUsers (): Promise<UserData[]> {
     return this.repository
   }
 
